2547c383a2c2bc2e26ca5304fa507d5b099496a3
emi~ <esauvisky@gmail.com>
2019-10-06 04:13:10 -0300

    Improving update.sh

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -4,8 +4,13 @@ set -o errtrace
 set -o pipefail # Exit on errors
 
 for i in ./.*; do
-    if [[ -f ~/"${i//\.\//}" && $(md5sum ~/"${i//\.\//}" | cut -d' ' -f1) != $(md5sum "$i" | cut -d' ' -f1) ]]; then
-        rm "$i"
-        cp ~/"${i//\.\//}" ./
+    if [[ -e ~/"${i//\.\//}" && $(md5sum ~/"${i//\.\//}" | cut -d' ' -f1) != $(md5sum "$i" | cut -d' ' -f1) ]]; then
+        if [[ -f ~/"${i//\.\//}" ]]; then
+            echo 'Whops, file '~/"${i//\.\//}"' is not a symlink to this repo! Check it out manually!'
+            return 1
+        fi
+        echo 'Updating ~/'"${i//\.\//}..."
+        gio trash ~/"${i//\.\//}"
+        ln -s ./ ~/"${i//\.\//}"
     fi
 done
