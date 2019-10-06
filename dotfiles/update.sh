9d5f89f119524a8bbac92011ceb4d269f6f9b157
emi~ <esauvisky@gmail.com>
2019-10-06 04:30:38 -0300

    Fixing update script

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -4,13 +4,13 @@ set -o errtrace
 set -o pipefail # Exit on errors
 
 for i in ./.*; do
-    if [[ -e ~/"${i//\.\//}" && $(md5sum ~/"${i//\.\//}" | cut -d' ' -f1) != $(md5sum "$i" | cut -d' ' -f1) ]]; then
-        if [[ -f ~/"${i//\.\//}" ]]; then
-            echo 'Whops, file '~/"${i//\.\//}"' is not a symlink to this repo! Check it out manually!'
-            return 1
-        fi
+    if [[ -d ${i//\.\//} && (${i//\.\//} == '.' || ${i//\.\//} == '..') ]]; then
+        continue
+    fi
+    if [[ -d ~/"${i//\.\//}" || $(md5sum ~/"${i//\.\//}" | cut -d' ' -f1) != $(md5sum "$i" | cut -d' ' -f1) || ( -e ~/"${i//\.\//}" && -L ~/"${i//\.\//}" ) ]]; then
+        echo 'The files arent the same! Probably not a symlink.'
         echo 'Updating ~/'"${i//\.\//}..."
-        gio trash ~/"${i//\.\//}"
-        ln -s ./ ~/"${i//\.\//}"
+        gio trash ~/"${i//\.\//}" || true
+        ln -s "$PWD/${i//\.\//}" ~/"${i//\.\//}"
     fi
 done
