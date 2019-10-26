f9d7c9d90b3ee68e7526dadb94fe2e2b9855a5a5
emi~ <esauvisky@gmail.com>
2019-10-25 22:49:18 -0300

    Fixing a regression on inputrc when menu-cycle-reverse with shift+tab

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -5,12 +5,18 @@ set -o pipefail # Exit on errors
 
 for i in ./.*; do
     if [[ -d ${i//\.\//} && (${i//\.\//} == '.' || ${i//\.\//} == '..') ]]; then
+        echo "Skipping $i"
         continue
     fi
-    if [[ -d ~/"${i//\.\//}" || $(md5sum ~/"${i//\.\//}" | cut -d' ' -f1) != $(md5sum "$i" | cut -d' ' -f1) || ( -e ~/"${i//\.\//}" && -L ~/"${i//\.\//}" ) ]]; then
-        echo 'The files arent the same! Probably not a symlink.'
+    if [[ -d ~/"${i//\.\//}"
+        || ($(md5sum ~/"${i//\.\//}" | cut -d' ' -f1) != $(md5sum "$i" | cut -d' ' -f1)) ]]; then
+        echo "The files ~/${i//\.\//} and ${i} arent the same or the latter is not a symlink."
+        echo "In case you need the file back, it's on your trash bin (deleted via gio trash, not rm)."
         echo 'Updating ~/'"${i//\.\//}..."
         gio trash ~/"${i//\.\//}" || true
         ln -s "$PWD/${i//\.\//}" ~/"${i//\.\//}"
+    fi
+    if [[ ( -e ~/"${i//\.\//}" && -L ~/"${i//\.\//}" ) ]]; then
+        echo "All good for $i"
     fi
 done
