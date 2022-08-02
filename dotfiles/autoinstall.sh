3917d5b52f2781ca7edcceb36191b21a7eb8812d
emi~ <esauvisky@gmail.com>
2022-08-02 03:27:22 -0300

    improve autoinstall

--- dotfiles/autoinstall.sh
+++ dotfiles/autoinstall.sh
@@ -7,54 +7,58 @@ set -o pipefail # Exit on errors
 __deps=("sed" "grep")
 for dep in "${__deps[@]}"; do hash $dep >&/dev/null || (echo "$dep was not found. Please install it and try again." && exit 1); done
 
-
 if [[ ! -t 0 ]]; then
     echo -e '\n\nPlease run like this instead:\nbash -c "$(curl -sSL https://raw.githubusercontent.com/esauvisky/arch-install/master/dotfiles/autoinstall.sh)"' && exit 1
 fi
 
 dotfiles=(".bashrc" ".bash_completion" ".dircolors" ".inputrc" ".toprc" ".dircolors")
+grcconfs=("conf.ant" "conf.blkid" "conf.common" "conf.configure" "conf.curl" "conf.cvs" "conf.df" "conf.diff" "conf.dig" "conf.dnf" "conf.dockerimages" "conf.dockerinfo" "conf.docker-machinels" "conf.dockernetwork" "conf.dockerps" "conf.dockerpull" "conf.dockersearch" "conf.dockerversion" "conf.du" "conf.dummy" "conf.efibootmgr" "conf.env" "conf.esperanto" "conf.fdisk" "conf.findmnt" "conf.free" "conf.gcc" "conf.getfacl" "conf.getsebool" "conf.go-test" "conf.id" "conf.ifconfig" "conf.iostat_sar" "conf.ip" "conf.ipaddr" "conf.ipneighbor" "conf.iproute" "conf.iptables" "conf.irclog" "conf.iwconfig" "conf.jobs" "conf.kubectl" "conf.last" "conf.ldap" "conf.log" "conf.ls" "conf.lsattr" "conf.lsblk" "conf.lsmod" "conf.lsof" "conf.lspci" "conf.mount" "conf.mtr" "conf.mvn" "conf.netstat" "conf.nmap" "conf.ntpdate" "conf.php" "conf.ping" "conf.ping2" "conf.proftpd" "conf.ps" "conf.pv" "conf.semanageboolean" "conf.semanagefcontext" "conf.semanageuser" "conf.sensors" "conf.showmount" "conf.sockstat" "conf.sql" "conf.ss" "conf.stat" "conf.sysctl" "conf.systemctl" "conf.tcpdump" "conf.traceroute" "conf.tune2fs" "conf.ulimit" "conf.uptime" "conf.vmstat" "conf.wdiff" "conf.whois" "conf.yaml" "grc.conf")
 
 cd "$HOME"
 
-if hash gio 2>/dev/null; then
-    # has gio support, can send older scripts to trash
-    for dep in "${dotfiles[@]}"; do
-        echo "Downloading script $dep from esauvisky/arch-install/master/dotfiles"
-        url="https://raw.githubusercontent.com/esauvisky/arch-install/master/dotfiles/$dep"
+echo "Downloading scripts from esauvisky/arch-install/master/dotfiles"
+for dep in "${dotfiles[@]}"; do
+    url="https://raw.githubusercontent.com/esauvisky/arch-install/master/dotfiles/$dep"
 
-        if hash wget 2>/dev/null; then
-            wget -q "$url" -O "$dep.1" 2>/dev/null
-        else
-            curl -o "$dep.1" "$url" 2>/dev/null
-        fi
+    if hash wget 2>/dev/null; then
+        wget -q "$url" -O "$dep.1" 2>/dev/null
+    else
+        curl -o "$dep.1" "$url" 2>/dev/null
+    fi
 
+    if hash gio 2>/dev/null; then
         gio trash -f "./$dep"
-        mv "./$dep.1" "./$dep"
-
-        if [[ -f "./$dep" ]]; then
-            echo -e "All good! Moving on...\n"
-        fi
-    done
-    echo -e "Everything went well! Previous files (if any) were backed up with a .bak extension! Buh-bye!"
-else
-    # no gio support, better backup in place
-    for dep in "${dotfiles[@]}"; do
-        echo "Downloading script $dep from esauvisky/arch-install/master/dotfiles"
-        url="https://raw.githubusercontent.com/esauvisky/arch-install/master/dotfiles/$dep"
-
-        if hash wget 2>/dev/null; then
-            wget -q "$url" -O "$dep.1" 2>/dev/null
-        else
-            curl -o "$dep.1" "$url" 2>/dev/null
-        fi
-
+    else
         mv "./$dep" "./$dep.bak" 2>/dev/null || true
-        mv "./$dep.1" "./$dep"
-        if [[ -f "./$dep" ]]; then
-            echo -e "All good! Moving on...\n"
-        fi
-    done
-    echo -e "Everything went well! Previous files (if any) were backed up with a .bak extension! Buh-bye!"
+    fi
+
+    mv "./$dep.1" "./$dep"
+done
+
+echo "Downloading GRC config files from esauvisky/arch-install/master/dotfiles/grc"
+for conf in "${grcconfs[@]}"; do
+    url="https://raw.githubusercontent.com/esauvisky/arch-install/master/dotfiles/grc/$conf"
+    mkdir -p .grc
+
+    if hash wget 2>/dev/null; then
+        wget -q "$url" -O ".grc/$conf.1" 2>/dev/null
+    else
+        curl -o ".grc/$conf.1" "$url" 2>/dev/null
+    fi
+
+    if hash gio 2>/dev/null; then
+        gio trash -f ".grc/$dep"
+    else
+        mv "./$dep" "./$dep.bak" 2>/dev/null || true
+    fi
+
+    mv ".grc/$conf.1" ".grc/$conf"
+done
+
+if hash gio 2>/dev/null; then
+    echo -e "Everything went well! Previous files are in the trash bin."
+else
+    echo -e "Everything went well! Previous files (if any) were backed up with a .bak extension!"
 fi
 
 if hash pacman 2>/dev/null; then
@@ -71,4 +75,5 @@ elif hash apt 2>/dev/null; then
     fi
 fi
 
+source ./.bashrc
 echo -e "\n That's all! KTHXBYE"
